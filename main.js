80fbae6b93e7df7f4af7b0f86dc1328ec4f491cf
wisdan31 <84465475+wisdan31@users.noreply.github.com>
2023-10-05 11:07:17 +0300

    completed lab

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-//test
+const fs = require("node:fs");
+fs.readFile("data.json", (err, data) => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        let jsonData = JSON.parse(data);
+        let max = -Infinity;
+        for (let i = 0; i < jsonData.length; i++){
+            if (jsonData[i].rate > max) max = jsonData[i].rate;
+        }
+
+        let rateString = "Максимальний курс:" + max;
+        
+        fs.writeFile('output.txt', rateString, (err) => {
+            if (err){
+                console.log(err);
+            }
+            else {
+                console.log(rateString);
+            }
+        })
+    }
+});
